7126081f53901bad1ae81238b71ac0a056c72774
Niklas Närhinen <niklas@narhinen.net>
2018-11-29 10:18:57 +0200

    Fix core subtitle extension clashing with this

--- flowplayer.hls-plugin-subtitles.js
+++ flowplayer.hls-plugin-subtitles.js
@@ -7,22 +7,35 @@ flowplayer.engine('hlsjs-lite').plugin(function(data) {
   var tracks = []
     , activeTrack;
 
-  videoTag.textTracks.addEventListener('addtrack', function(ev) {
-    if (ev.track.kind !== 'captions') return;
+  data.hls.subtitleDisplay = false;
 
-    tracks = [].filter.call(videoTag.textTracks, function(tr) { return tr.kind === 'captions'; });
-
+  function createControl(tracks) {
     player.ui.createSubtitleControl(tracks, function(idx) {
       if (Number(idx) === -1) {
+        if (activeTrack) activeTrack.mode = 'disabled';
         activeTrack = null;
         player.hideSubtitle();
       }
       else activeTrack = tracks[idx];
       player.ui.setActiveSubtitleItem(idx);
+
+      if (activeTrack) activeTrack.mode = 'hidden';
+    }, {
+      controlledExternally: true
     });
 
     player.ui.setActiveSubtitleItem(-1);
+  }
 
+  videoTag.textTracks.addEventListener('addtrack', function(ev) {
+    if (ev.track.kind !== 'captions' && ev.track.kind !== 'subtitles') return;
+
+    tracks = [].filter.call(videoTag.textTracks, function(tr) { return tr.kind === 'captions' || tr.kind === 'subtitles'; });
+
+    tracks.forEach(function(tr) { tr.mode = 'disabled'; });
+
+    if (player.ready) createControl(tracks);
+    else player.one('ready', function() { createControl(tracks); });
 
     ev.track.addEventListener('cuechange', function() {
       if (ev.track !== activeTrack) return;
